abf11cef63fb5ec80e978825cd7599b3887a9c86
rakibsultan <nihan.khan@outlook.com>
2021-11-11 08:33:14 +0600

    This is a new project

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("./Static/")))
+	//http.Handle("/", http.FileServer(http.Dir("/.Static")))
+
+	fmt.Println("Http File Server Running...")
+
+	log.Fatal(http.ListenAndServe(":8002", nil))
+}
+
